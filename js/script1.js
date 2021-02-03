638a793b981530789e7642e36fc8053a8bddb596
nhaingo <nah21001@byu.edu>
2021-02-03 07:49:18 +0300

    poundsToKilograms Final Change

--- js/script1.js
+++ js/script1.js
@@ -2,14 +2,18 @@
  INPUT:  
 Get the pound value to convert to kilogram in the input box – x 
  
- OUTPUT: Display the converted value in kilograms
+ OUTPUT: Display the converted value in kilograms and rounded to one decimal
 */ 
 
 function convertToKilograms() {
-    //INPUT
+    //INPUT A POUNDS VALUE FROM USER
     let x = parseFloat(document.getElementById('pounds').value)
-    //OUTPUT
+    //PROCESS: CONVERT THE USER INPUT INTO A NUMBER
+    let lbs = parseFloat(x);
+    //PROCESS: CONVERT THE USER INPUT TO KILOGRAMS, ROUNDED TO ONE DECIMAL
+    let kilograms = Math.round((x*0.453592)*10)/10;
+    //OUTPUT: DISPLAY THE POUNDS VALUE TO KILOGRAMS
 	
-    document.getElementById('outputKilograms').innerHTML = x*0.453;
+    document.getElementById('outputKilograms').innerHTML = kilograms;
 
 }
